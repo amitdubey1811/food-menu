659aa0d977a760d69786906e741e10bddac14ceb
amitdubey1811 <67070714+amitdubey1811@users.noreply.github.com>
2022-07-25 01:28:41 +0530

    adding files

---
+++ src/components/basics/Menuapi.js
@@ -0,0 +1,130 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    
+    {
+        id: 10,
+        image: "../images/poha.jpg",
+        name: "Poha",
+        category: "breakfast",
+        price: "10₹",
+        description:
+          "Pohaa, also known as pauwa, sira, chira, or avalakki, among many other names, is flattened rice originating from the Indian subcontinent. Rice is parboiled before flattening so that it can be consumed with very little to no cooking."
+        }
+    ,
+      {
+        id: 11,
+        image: "../images/grain.jpg",
+        name: "Grain-Waffles",
+        category: "breakfast",
+        price: "10₹",
+        description:
+          "Frozen waffles are an easy, delicious swap for toast. Look for water or 100% whole grains as the first ingredient, and keep the added sugar content as low as possible. Kashi Whole-Grain Waffles are filled with fiber and protein and contain just 3 grams of sugar for two.",
+      },
+      {
+        id: 12,
+        image: "../images/litti.jpg",
+        name: "Litti Chokha",
+        category: "dinner",
+        price: "10₹",
+        description:
+          "a rustic and traditional dish of stuffed whole wheat dough balls, where the stuffing is a spiced mix of roasted gram flour or sattu. this recipe comes from bihar and jharkand region in India.",
+      },
+      {
+        id: 13,
+        image: "../images/paneer.jpg",
+        name: "Paneer Tikka",
+        category: "dinner",
+        price: "10₹",
+        description:
+          "a rustic and traditional dish of stuffed whole wheat dough balls, where the stuffing is a spiced mix of roasted gram flour or sattu. this recipe comes from bihar and jharkand region in India.",
+      
+
+
+      }
+
+
+  ];
+  
+  export default Menu
+  //array of an object
